015badef3472c5e1e5ad5e0257b17a0b62806730
jordanzitorr <jordanzitorr@gmail.com>
2023-04-10 19:38:01 +0200

    contact done

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import { Points, PointMaterial, Preload } from "@react-three/drei"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { useState, useRef, Suspense } from "react"
+
+
+
 
 const Stars = () => {
   return (
